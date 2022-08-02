504d3ae133a75c7299193ce1bb5ced6f671a8c0d
Alistair Tait <Alistair.Tait@bglgroup.co.uk>
2022-08-02 23:00:26 +0100

    Added Ingredient model, created database context, updated launch settings

---
+++ YummyrDataApi/Models/YummyrContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace YummyrDataApi.Models
+{
+    public class YummyrContext : DbContext
+    {
+        public YummyrContext(DbContextOptions<YummyrContext> options)
+            :base(options)
+        {
+        }
+
+        public DbSet<Ingredient>? Ingredients { get; set; } = null;
+    }
+}
