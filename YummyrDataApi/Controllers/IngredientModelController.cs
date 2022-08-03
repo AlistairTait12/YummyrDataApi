5f0e91ea71a02cf887c736a8cdfe3fd144a151db
Alistair Tait <Alistair.Tait@bglgroup.co.uk>
2022-08-03 15:29:45 +0100

    Added IngredientModelController and scaffolded IngredientController for eductional purposes

---
+++ YummyrDataApi/Controllers/IngredientModelController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YummyrDataApi.Models;
+
+namespace YummyrDataApi.Controllers
+{
+    [Route("api/ingredients")]
+    public class IngredientModelController : ControllerBase
+    {
+        private readonly YummyrContext _context;
+
+        public IngredientModelController(YummyrContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Ingredients/5
+        [HttpGet]
+        public async Task<ActionResult<IngredientModel>> GetIngredient(int id)
+        {
+            if (_context.Ingredients is null)
+            {
+                return NotFound();
+            }
+
+            var ingredient = await _context.Ingredients.FindAsync(id);
+            var dietaryInfo = _context.DietaryInfo.Where(info => info.IngredientId == id);
+
+            return new IngredientModel
+            {
+                IngredientName = ingredient.Name,
+                DietaryTypes = dietaryInfo.Select(info => info.DietaryType).ToList()
+            };
+        }
+    }
+}
--- YummyrDataApi/Models/IngredientModel.cs
+++ YummyrDataApi/Models/IngredientModel.cs
@@ -2,6 +2,7 @@
 {
     public class IngredientModel
     {
+        public int Id { get; set; }
         public string? IngredientName { get; set; }
         public IEnumerable<DietaryType>? DietaryTypes { get; set; }
     }
