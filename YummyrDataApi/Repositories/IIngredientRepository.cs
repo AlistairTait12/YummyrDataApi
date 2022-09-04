d7096b05f0a9bbe901f1933b54d678b09b235b4c
Alistair Tait <Alistair.Tait@bglgroup.co.uk>
2022-09-04 21:39:59 +0100

    trialling out creating a repository for db abstraction

---
+++ YummyrDataApi/Repositories/IIngredientRepository.cs
@@ -0,0 +1,9 @@
+﻿using YummyrDataApi.Models;
+
+namespace YummyrDataApi.Repositories
+{
+    public interface IIngredientRepository
+    {
+        IEnumerable<Ingredient> GetIngredientsBeginningWith(char letter);
+    }
+}
---
+++ YummyrDataApi/Repositories/IngredientRepository.cs
@@ -0,0 +1,17 @@
+﻿using YummyrDataApi.Models;
+
+namespace YummyrDataApi.Repositories
+{
+    public class IngredientRepository : Repository<Ingredient>, IIngredientRepository
+    {
+        public IngredientRepository(YummyrContext context)
+            : base(context)
+        {
+        }
+
+        public IEnumerable<Ingredient> GetIngredientsBeginningWith(char letter)
+        {
+            return new List<Ingredient>();
+        }
+    }
+}
