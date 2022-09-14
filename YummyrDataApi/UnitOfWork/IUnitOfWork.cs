d93ea7b7313a9e6ad2b93851a902632c0f7f21fc
Alistair Tait <Alistair.Tait@bglgroup.co.uk>
2022-09-14 16:41:49 +0100

    Added IUnitOfWork and its implementation to the codebase

---
+++ YummyrDataApi/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using YummyrDataApi.Repositories;
+
+namespace YummyrDataApi.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IIngredientRepository Ingredients { get; }
+
+        int Complete();
+    }
+}
---
+++ YummyrDataApi/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using YummyrDataApi.Models;
+using YummyrDataApi.Repositories;
+
+namespace YummyrDataApi.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly YummyrContext _context;
+
+        public UnitOfWork(YummyrContext context)
+        {
+            _context = context;
+            Ingredients = new IngredientRepository(_context);
+        }
+
+        public IIngredientRepository Ingredients { get; private set; }
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
